46989dc33cac6db2200a6a7f7cb7d2c995ca71ed
Serghei28 <serghei.petis@isa.utm.md>
2025-09-08 14:12:30 +0300

    Modal components (Admin)

---
+++ components/ui/modal.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { DialogDescription, 
+    DialogTitle, 
+    Dialog, 
+    DialogContent, 
+    DialogHeader 
+} from "@/components/ui/dialog"
+
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+  title: string
+  description: string
+  children?: React.ReactNode
+}
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+    title,
+    description,
+    children
+}) => {
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose()
+        }
+    }
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                <div>
+                {children}
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
